9ff48118e25de5719eb82b6c6a8995338a36b4f9
priyaldave5 <priyaldave007@gmail.com>
2024-11-30 21:27:31 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import random
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("💬Chatbot")
 st.write(
     "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "

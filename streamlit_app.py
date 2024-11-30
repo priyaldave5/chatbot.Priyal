69ace8d34d6cb4353f7bcae3c1685d1218ea9bc7
priyaldave5 <priyaldave007@gmail.com>
2024-11-30 21:29:26 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,9 +8,7 @@ from sklearn.linear_model import LogisticRegression
 # Show title and description.
 st.title("💬Chatbot")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "This is a simple chatbot created by Priyal."
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
